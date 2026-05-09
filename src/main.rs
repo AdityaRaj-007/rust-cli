6665339578ddbbca6e541320e6a3abd62f2214a6
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2026-05-09 12:56:38 +0530

    added cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use clap::{Parser, Subcommand};
+use chrono::{Local};
+use uuid::Uuid;
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct ExpenseTracker {
+    #[command(subcommand)]
+    command: Commands
+}
+
+struct Expense {
+    category: String,
+    amount: f64,
+    date: String,
+    id: String
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Add {
+        #[arg(short, long)]
+        category:String,
+
+        #[arg(short, long)]
+        amount: f64
+    },
+    Delete{
+        #[arg(short, long)]
+        id: String
+    },
+    List 
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = ExpenseTracker::parse();
+
+    let mut expenses: Vec<Expense> = Vec::new();
+
+    match &cli.command {
+        Commands::Add { category, amount } => {
+            println!("Adding Expense - Category: {}, Amount: {}", category, amount);
+
+            let todays_date = Local::now().format("%Y-%m-%d").to_string();
+
+            let expense = Expense {
+                category: category.to_string(),
+                amount: *amount,
+                date: todays_date,
+                id: Uuid::new_v4().to_string()
+            };
+
+            expenses.push(expense);
+        }
+        Commands::Delete { id } => {
+            println!("Deleting expense id: {}", id)
+        }  
+        Commands::List => {
+            println!("Lists all the expenses...");
+            
+            let expense_iter = expenses.iter();
+
+            for expense in expense_iter {
+                println!("Id: {}, Category: {}, Amount: {}, Date: {}", expense.id, expense.category, expense.amount, expense.date);
+            }
+        }
+    }
 }
