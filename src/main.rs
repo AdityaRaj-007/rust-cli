55bee8efefc9071852af42047b579e2a0f0b7a30
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2026-05-09 18:13:30 +0530

    added add , delete, and list functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::{Parser, Subcommand};
 use chrono::{Local};
 use uuid::Uuid;
+use core::panic;
 use std::fs::File;
+use std::fs;
+use std::io::{ Write};
 use std::path::Path;
+use serde::{Serialize, Deserialize};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -11,6 +15,7 @@ struct ExpenseTracker {
     command: Commands
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Expense {
     category: String,
     amount: f64,
@@ -34,10 +39,42 @@ enum Commands {
     List 
 }
 
+fn add_expense(file_name: String, expenses: Vec<Expense>) {
+    let path = Path::new(&file_name);
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("Unable to open {} in write mode: {}", display, why),
+        Ok(file) => file 
+    };
+
+    let serialized_data = serde_json::to_string_pretty(&expenses).unwrap();
+
+    match file.write_all(serialized_data.as_bytes())  {
+        Err(why ) => panic!("Unable to add expense to {}: {}", display, why),
+        Ok(_) => println!("Expenses saved successfully...")
+    }
+}
+
+fn get_all_expense() -> Vec<Expense> {
+    let path = Path::new("Expenses.json");
+    if !path.exists() {
+        return Vec::new();
+    }
+    let data = match fs::read_to_string(&path) {
+        Ok(content) => content,
+        Err(why) => panic!("Unable to read data from {}: {}", path.display(), why),
+    };
+
+    let deserialized_data = serde_json::from_str(&data).unwrap_or_else(|_| Vec::new());
+
+    deserialized_data
+}
+
 fn main() {
     let cli = ExpenseTracker::parse();
 
-    let mut expenses: Vec<Expense> = Vec::new();
+    let mut expenses = get_all_expense();
 
     match &cli.command {
         Commands::Add { category, amount } => {
@@ -50,20 +87,29 @@ fn main() {
                 amount: *amount,
                 date: todays_date,
                 id: Uuid::new_v4().to_string()
-            };
+            };      
 
-            expenses.push(expense);
+            expenses.push(expense);      
+
+            add_expense("Expenses.json".to_string(), expenses);
         }
         Commands::Delete { id } => {
-            println!("Deleting expense id: {}", id)
-        }  
-        Commands::List => {
-            println!("Lists all the expenses...");
-            
-            let expense_iter = expenses.iter();
+            println!("Deleting expense id: {}", id);
 
-            for expense in expense_iter {
-                println!("Id: {}, Category: {}, Amount: {}, Date: {}", expense.id, expense.category, expense.amount, expense.date);
+            let expenses_iter = expenses.into_iter();
+            let new_expenses = expenses_iter.filter(|expense| expense.id != *id).collect();
+
+            add_expense("Expenses.json".to_string(), new_expenses);
+        }  
+        Commands::List => {            
+            if expenses.is_empty() {
+                println!("No expenses...")
+            } else {
+                println!("Expenses...");
+
+                for expense in expenses {
+                    println!("Id: {}, Category: {}, Amount: {}, Date: {}", expense.id, expense.category, expense.amount, expense.date);
+                }
             }
         }
     }
